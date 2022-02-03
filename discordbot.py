1acf578b99d43d82734f06a955f701af69f59c27
RuneNight <92235330+RuneNight@users.noreply.github.com>
2022-02-03 21:29:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,27 +1,9 @@
 from discord.ext import commands
 from os import getenv
-import discord
 import traceback
+
 bot = commands.Bot(command_prefix='/')
-client = discord.Client()
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('にゃーん')
-
-# Botの起動とDiscordサーバーへの接続
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -35,5 +17,5 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
-token = OTM3MjI3NzM4ODUzNTUyMjI5.YfYrWQ.27RCwcCYAKvZUNa0mSzfq-4h1gQ
+token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
